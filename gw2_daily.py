cb8f1ce03524ffe711346b2b54622540476abdad
Yoshi <KingXiaoDi@users.noreply.github.com>
2017-05-10 14:30:36 -0400

    Update gw2_daily.py

--- gw2_daily.py
+++ gw2_daily.py
@@ -4,61 +4,79 @@ import datetime
 import time
 
 def get_daily_info(*styles, show_all=False):
-	id_list = get_dailies(*styles)
-	url = 'https://api.guildwars2.com/v2/achievements?ids=' + ','.join(str(x) for x in id_list['IDs'])
-	page = urllib.request.urlopen(url).read().decode('utf-8', 'ignore')
-	if '—' in page:
-		page = page.replace('—', '-')
-	data = json.loads(page)
-	quest_name = []
-	skipped_id = []
-	skipped_name = []
-	skipped_counter = 0
-	for quest in data:
-		if 'Tier' in quest['name'] and '4' not in quest['name']:
-			skipped_id.append(id_list['IDs'].pop())
-			skipped_name.append(quest['name'])
-			skipped_counter += 1
-		else:
-			quest_name.append(quest['name'])
-	assert len(quest_name) == len(id_list['IDs']), "Warning: Lists of ID and name are not the same length. len(ID): "+str(len(id_list['IDs']))+"\t len(Name): "+str(len(quest_name))
-	assert len(skipped_name) == len(skipped_id), "Warning: Lists of skipped ID and skipped name are not the same length. len(Skipped ID): "+str(len(skipped_id))+"\t len(Skipped Name): "+str(len(skipped_name))
-	print ("GW2 Dailies ("+str(len(id_list['IDs']))+")"+check_time()+":")
-	for id, quest in zip(id_list['IDs'], quest_name):
-		print ('ID: '+'{:4d}\t {}'.format(id, quest))
-	print ()
-	if show_all == True:
-		print (str(skipped_counter)+" dailies separated due to Tier level <> 4.")
-		for x, y in zip(skipped_id, skipped_name):
-			print ('ID: '+'{:4d}\t {}'.format(x, y))
-	else:
-		print ("{} dailies removed due to Tier level <> 4. Include 'show_all=True' to show all dailies.".format(skipped_counter))
-
+	id_dict = get_dailies(*styles)
+	quest_name = {}
+	skipped_id = {}
+	skipped_quest_name = {}
+	ordered = ['pve','pvp','wvw','fractals','special']
+	print ("GW2 Dailies\t"+check_time())
+	for style in ordered:
+		skipped_id[style] = []
+		skipped_quest_name[style] = []
+		quest_name[style] = []
+		if id_dict[style] != []:
+			url = 'https://api.guildwars2.com/v2/achievements?ids=' + ','.join(str(x) for x in id_dict[style])
+			page = urllib.request.urlopen(url).read().decode('utf-8', 'ignore')
+			page = page.replace('—', '-')
+			data = json.loads(page)
+			for quest in data:
+				if 'Tier' in quest['name'] and '4' not in quest['name']:
+					skipped_id[style].append(id_dict[style].pop())
+					skipped_quest_name[style].append(quest['name'])
+				else:
+					quest_name[style].append(quest['name'])
+	
+			assert len(quest_name[style]) == len(id_dict[style]), "Warning: Lists of ID and name are not the same length. Type: "+style+"\tlen(ID): "+str(len(quest_name[style]))+"\t len(Name): "+str(len(id_dict[style]))
+			assert len(skipped_quest_name[style]) == len(skipped_id[style]), "Warning: Lists of skipped ID and skipped name are not the same length. Type: "+style+"\t len(Skipped Name): "+str(len(skipped_name[style])) + "\t len(Skipped ID): "+str(len(skipped_id[style]))
+			if len(skipped_id[style]) > 0:
+				print ("\t{} ({}) [{} skipped]".format(style.capitalize(), len(id_dict[style]), len(skipped_id[style])))
+			else:
+				print ("\t{} ({})".format(style.capitalize(), len(id_dict[style])))
+			for id, quest in zip(id_dict[style], quest_name[style]):
+				print ('ID: '+'{:4d}\t {}'.format(id, quest))
+			print ()
+			if style == 'fractals':
+				if show_all == True:
+					print ("{} {} dailies separated due to tier level <> 4.".format(len(skipped_id[style]), style.capitalize()))
+					for x, y in zip(skipped_id[style], skipped_quest_name[style]):
+						print ('ID: '+'{:4d}\t {}'.format(x, y))
+					print ()
+				else:
+					print ("{} dailies removed due to tier level <> 4. Include 'show_all=True' to show all dailies.".format(len(skipped_id[style])))
+					print ()
+					
 def get_dailies(*styles):
 	dailies = json.loads(urllib.request.urlopen('https://api.guildwars2.com/v2/achievements/daily').read().decode('utf-8'))
-	id_list = {'IDs': []}
-	quest_types = {'pvp': [], 'pve': [], 'wvw': [], 'fractals': [], 'special': []}
-	accepted = ['pvp', 'pve', 'wvw', 'fractals', 'special']
+	daily_id_dict = {'pvp': [], 'pve': [], 'wvw': [], 'fractals': [], 'special': []}
 	if styles == ():
-		order = ["pve", 'pvp', 'wvw', 'fractals', 'special']
-		for x in order:
-			for y in sorted(dailies[x], key = lambda x: x['id']):
-				if y['id'] not in id_list['IDs']:
-					id_list['IDs'].append(y['id'])
-		id_list['IDs'].sort()
-		return (id_list)
-#need a way to reject bad input'''
+		return empty_search(dailies, daily_id_dict)
 	else:
-		try:
-			for style in styles:
-				if style.lower() in quest_types.keys():
-					for x in dailies[style]:
-						if x['id'] not in quest_types[style]:
-							quest_types[style].append(x['id'])
-					quest_types[style].sort()
-		except:
-			print ("fail")
-
+		maxlength = 0
+		for style in styles:
+			if style.lower() in daily_id_dict.keys():
+				for x in dailies[style]:
+					if x['id'] not in daily_id_dict[style]:
+						daily_id_dict[style].append(x['id'])
+					daily_id_dict[style].sort()
+				maxlength = max(maxlength, len(daily_id_dict[style]))
+			else:
+				print ("Sorry, I didn't understand '{}'".format(style))
+		if maxlength == 0:
+			print ("Sorry, I couldn't understand any of the quest type(s) you entered: "+', '.join(x for x in styles))
+			print ("Since you didn't pass me a useable quest type ("+', '.join(x for x in daily_id_dict.keys())+"), I will return all dailies.")
+			print ()
+			return empty_search(dailies, daily_id_dict)	
+		else:
+			return daily_id_dict
+			
+def empty_search(dailies, daily_id_dict):
+	for style in daily_id_dict.keys():
+			for x in dailies[style]:
+				if x['id'] not in daily_id_dict[style]:
+					daily_id_dict[style].append(x['id'])
+					daily_id_dict[style].sort()
+	return (daily_id_dict)
+		
 def check_time():
 	diff = int(time.strftime('%z'))
 	hours_diff = diff/100
